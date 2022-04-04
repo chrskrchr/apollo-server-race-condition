23f4a36f443a3af7cdbe411ec6ed78c3cfa45799
Chris Karcher <chris.karcher@care.com>
2022-04-04 11:24:28 -0500

    add log statements for sleep delay

--- resolvers.js
+++ resolvers.js
@@ -6,15 +6,17 @@ function generateFieldValueFn(fieldName, seed) {
   console.log(`field=${fieldName} seed=${seed}`);
   return async () => {
     const mode = Number(seed % 100n);
-    const delayMs = (Number(seed % 100n) / 100) * 100;
+    const delayMs = (Number(seed % 1000n) / 1000) * 100;
     if (mode < 45) {
       return "bar sync";
     } else if (mode < 90) {
+      console.log(`Sleeping ${delayMs}ms`);
       await delay(delayMs);
       return "bar async";
     } else if (mode < 93) {
       throw new Error("error sync");
     } else if (mode < 96) {
+      console.log(`Sleeping ${delayMs}ms`);
       await delay(delayMs);
       throw new Error("error async");
     } else {
